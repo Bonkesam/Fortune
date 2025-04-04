f371d5f4b9cab52375dc22543c47ae6b4106170d
Bonkesam <bonkesam9@gmail.com>
2025-04-04 18:18:17 +0300

    Adds  the chainlink interface

---
+++ src/interfaces/IChainlinkVRF.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title IChainlinkVRF
+ * @notice Interface for Chainlink VRF v2
+ * @dev Used in Randomness.sol
+ */
+interface IChainlinkVRF {
+    /**
+     * @notice Request randomness
+     * @param keyHash Key hash for oracle job
+     * @param subId Subscription ID
+     * @param minConfs Minimum confirmations
+     * @param callbackGasLimit Gas limit for callback
+     * @param numWords Number of random values
+     * @return requestId Generated request ID
+     */
+    function requestRandomWords(
+        bytes32 keyHash,
+        uint64 subId,
+        uint16 minConfs,
+        uint32 callbackGasLimit,
+        uint32 numWords
+    ) external returns (uint256 requestId);
+
+    /**
+     * @notice Check request status
+     * @param requestId ID to check
+     * @return fulfilled True if request completed
+     * @return randomWords Array of random numbers
+     */
+    function getRequestStatus(
+        uint256 requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords);
+}
